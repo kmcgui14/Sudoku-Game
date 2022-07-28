ffb73deb16cb0e033c9b1a56bd0e1c6761e0ba71
kmcgui14 <kmcgui14@students.kennesaw.edu>
2022-07-28 12:37:03 -0400

    added earth tones to UI, fixed issues with selecting difficulty

--- sudoku.py
+++ sudoku.py
@@ -1,30 +1,26 @@
 import pygame
 import pygame_menu
 from pygame_menu.examples import create_example_window
+import pygame_menu.themes
 from typing import Tuple, Any
 import requests
 
-bg_color = (251, 247, 245)
+bg_color = (178, 166, 141)
 pygame.init()
 window = pygame.display.set_mode((550,550))
 pygame.display.set_caption("Sudoku")
 myfont = pygame.font.SysFont('Comic Sans MS', 35)
 surface = create_example_window('Sudoku', (550, 550))
 
-original_grid_element_color = (52, 31, 151)
+original_grid_element_color = (145, 102, 66)
 buffer = 5
 
-response = ""
-grid = ""
-grid_original = ""
+diff = input("Select Difficulty(Easy,Medium,Hard): ")
+url = "https://sugoku.herokuapp.com/board?difficulty=" + str(diff)
+response = requests.get(url, verify=False)
+grid = response.json()['board']
+grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
 difficulty = [1]
-
-def set_difficulty(selected: Tuple, value: Any):
-    """
-    Set the difficulty of the game.
-    """
-
-    difficulty[0] = value
 
 def insert(window, position):
     i,j = position[1], position[0]
@@ -43,7 +39,7 @@ def insert(window, position):
                     return
                 if(0 < event.key - 48 <10):  #We are checking for valid input
                     pygame.draw.rect(window, bg_color, (position[0]*50 + buffer, position[1]*50+ buffer,50 -2*buffer , 50 - 2*buffer))
-                    value = myfont.render(str(event.key-48), True, (0,0,0))
+                    value = myfont.render(str(event.key-48), True, (131, 108, 115))
                     window.blit(value, (position[0]*50 +15, position[1]*50))
                     grid[i-1][j-1] = event.key - 48
                     pygame.display.update()
@@ -52,21 +48,6 @@ def insert(window, position):
 
 def start_the_game():
     window.fill(bg_color)
-
-    # change difficulty
-    if (difficulty[0] == 1):
-        pygame.display.set_caption("Sudoku - Easy")
-        response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy", verify=False)
-    if (difficulty[0] == 2):
-        pygame.display.set_caption("Sudoku - Medium")
-        response = requests.get("https://sugoku.herokuapp.com/board?difficulty=medium", verify=False)
-    if (difficulty[0] == 3):
-        pygame.display.set_caption("Sudoku - Hard")
-        response = requests.get("https://sugoku.herokuapp.com/board?difficulty=hard", verify=False)
-    
-    grid = response.json()['board']
-    grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
-
 
     # displaying grid
     for i in range(0,10):
@@ -98,15 +79,17 @@ def start_the_game():
                 return
 
 
+mytheme = pygame_menu.Theme(background_color=(209, 156, 76),
+                title_background_color=(157, 95, 56))
+
 menu = pygame_menu.Menu(
     height=300,
-    theme=pygame_menu.themes.THEME_BLUE,
+    theme=mytheme,
     title='Sudoku',
     width=400
 )
 
 menu.add.button('Play', start_the_game)
-menu.add.selector('Difficulty: ', [('Easy', 1), ('Medium', 2), ('Hard', 3)], onchange=set_difficulty)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
 if __name__ == '__main__':
